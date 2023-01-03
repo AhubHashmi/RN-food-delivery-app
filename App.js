8da496b232a9b551452a459a7f1f8382bb11b6b3
Muhammad Ahub Hashmi <107793320+AhubHashmi@users.noreply.github.com>
2023-01-03 11:33:29 +0500

    updated!!

--- App.js
+++ App.js
@@ -6,6 +6,10 @@ import { NavigationContainer } from '@react-navigation/native'
 import { Restaurant, OrderDelivery } from './screens'
 import Tabs from './navigation/tabs'
 import GeoLocation from './screens/LiveGeoLocation';
+import Cart from './screens/Cart';
+import Splash from './screens/splashscreen';
+import Login from './screens/login';
+import SignUp from './screens/signup';
 
 const Stack = createStackNavigator();
 
@@ -13,18 +17,28 @@ const App = () => {
     return (
         <NavigationContainer>
             <Stack.Navigator
-                screenOptions={{
-                    headerShown: false
-                }}
-                initialRouteName={'Home'}
+            // screenOptions={{
+            //     headerShown: false
+            // }}
+            // initialRouteName={'Home'}             
             >
+                <Stack.Screen
+                    options={{
+                        headerShown: false,
+                    }}
+                    name="Splash"
+                    component={Splash}
+                />
                 <Stack.Screen name="Home" component={Tabs} />
+                <Stack.Screen name="Login" component={Login} />
+                <Stack.Screen name="SignUp" component={SignUp} />
                 <Stack.Screen name="Restaurant" component={Restaurant} />
                 <Stack.Screen name="OrderDelivery" component={OrderDelivery} />
                 <Stack.Screen name="LiveGeoLocation" component={GeoLocation} />
+                <Stack.Screen name="Cart" component={Cart} />
             </Stack.Navigator>
         </NavigationContainer>
-    )
+    );
 }
 
 export default App;
